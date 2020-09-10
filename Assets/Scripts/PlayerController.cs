d69e2e1967d73eef1b74213f3148cf4f0776b1f3
jcbrady419 <70285750+jcbrady419@users.noreply.github.com>
2020-09-10 13:53:21 -0400

    Adding test files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
+{
+    public float speed;
+    public RollABallControls controls;
+    private Rigidbody rb;
+    public Vector2 motion;
+    // Comment
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnEnable() {
+        if (controls == null)
+        {
+            controls = new RollABallControls();
+
+            controls.Player.SetCallbacks(this);
+        }
+        controls.Player.Enable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        motion = context.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(motion.x, 0.0f, motion.y);
+        rb.AddForce(movement*speed); 
+    }
+}
