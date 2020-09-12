ae307f9eb87178b2e187d64da8fc283d7e1291c8
jcbrady419 <70285750+jcbrady419@users.noreply.github.com>
2020-09-12 13:39:21 -0400

    Adding test files

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,13 +8,19 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
 {
     public float speed;
+    public Text countText;
+    public Text winText;
     public RollABallControls controls;
     private Rigidbody rb;
+    private int count;
     public Vector2 motion;
     // Comment
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
     }
 
     public void OnEnable() {
@@ -35,5 +42,22 @@ public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
     {
         Vector3 movement = new Vector3(motion.x, 0.0f, motion.y);
         rb.AddForce(movement*speed); 
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text= "Count: " + count.ToString ();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
