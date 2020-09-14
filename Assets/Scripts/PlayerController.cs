0376dffb4b9ba02ac85f00ae8846806188683130
jcbrady419 <70285750+jcbrady419@users.noreply.github.com>
2020-09-14 18:24:20 -0400

    submission on 14/09/2020 18:24:20

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public RollABallControls controls;
+    private Rigidbody rb;
+    private int count;
+    public Vector2 motion;
+    // Comment
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    public void OnEnable() {
+        if (controls == null)
+        {
+            controls = new RollABallControls();
+
+            controls.Player.SetCallbacks(this);
+        }
+        controls.Player.Enable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        motion = context.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(motion.x, 0.0f, motion.y);
+        rb.AddForce(movement*speed); 
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text= "Count: " + count.ToString ();
+        if (count >= 12)
+        {
+            winText.text = "You Win! Jacob Brady";
+        }
+    }
+}
