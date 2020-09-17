23949cc44e1341db9b1c8f5acbb8912d8ceb972e
jcbrady419 <70285750+jcbrady419@users.noreply.github.com>
2020-09-17 00:51:02 -0400

    Modified /Users/jacobbrady/Documents/GitHub/dig3480-f20-t1-jcbrady419/Assets/Scripts/Boxmover.cs

--- Assets/Scripts/Boxmover.cs
+++ Assets/Scripts/Boxmover.cs
@@ -38,3 +38,4 @@ void Update()
      // Set our position as a fraction of the distance between the markers and pingpong the movement.
           transform.position = Vector3.Lerp(startMarker.position, endMarker.position, Mathf.PingPong (fracJourney, 1));
      }
+}
