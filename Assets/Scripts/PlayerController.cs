149769bcd7eedb0897bc51c621aca696d8008a21
jcbrady419 <70285750+jcbrady419@users.noreply.github.com>
2020-09-17 00:06:54 -0400

    Modified /Users/jacobbrady/Documents/GitHub/dig3480-f20-t1-jcbrady419/Assets/Scripts/PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
         SetCountText ();
         winText.text = "";
         lives = 3;
-        SetLivesText ();
+        lives
     }
 
     public void OnEnable() {
