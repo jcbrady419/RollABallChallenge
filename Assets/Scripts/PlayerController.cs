c1af7f26f941afc18d61e8b809b2d1d7a309e0f6
jcbrady419 <70285750+jcbrady419@users.noreply.github.com>
2020-09-17 00:10:40 -0400

    Modified /Users/jacobbrady/Documents/GitHub/dig3480-f20-t1-jcbrady419/Assets/Scripts/PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -60,7 +60,7 @@ public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
         {
             other.gameObject.SetActive (false);
             lives = lives - 1;
-            livesText.text = ""
+            livesText.text = "Lives: "
         }
         if (count == 12)
         {
