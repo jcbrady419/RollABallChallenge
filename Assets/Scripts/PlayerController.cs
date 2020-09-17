52dbe671592f7bba5ed2e5c13f37b24be49b51b0
jcbrady419 <70285750+jcbrady419@users.noreply.github.com>
2020-09-17 00:06:05 -0400

    Added files since last session

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,9 +10,11 @@ public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
     public float speed;
     public Text countText;
     public Text winText;
+    public Text livesText;
     public RollABallControls controls;
     private Rigidbody rb;
     private int count;
+    private int lives;
     public Vector2 motion;
     // Comment
     void Start()
@@ -21,6 +23,8 @@ public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
         count = 0;
         SetCountText ();
         winText.text = "";
+        lives = 3;
+        SetLivesText ();
     }
 
     public void OnEnable() {
@@ -48,16 +52,31 @@ public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive (false);
-            count = count + 1;
+            int v = count + 1;
+            count = v;
             SetCountText ();
         }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive (false);
+            lives = lives - 1;
+            SetLivesText ();
+        }
+        if (count == 12)
+        {
+    transform.position = new Vector3(50.0f, 0.0f, 50.0f); 
+}
     }
     void SetCountText ()
     {
         countText.text= "Count: " + count.ToString ();
-        if (count >= 12)
+        if (count >= 20)
         {
             winText.text = "You Win! Jacob Brady";
         }
+    }
+    void SetLivesText ()
+    {
+        livesText.text= "Lives: " + count.ToString ();
     }
 }
